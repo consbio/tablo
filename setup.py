2eadc2f4187665a387acaf0d927feb8db04a8b82
MikeTheReader <mike@consbio.org>
2016-03-01 07:32:18 -0800

    Added packages to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='A PostGIS table to feature service app for Django',
     keywords='feature service, map server, postgis, django',
     version='0.1.0',
-    packages=['tablo'],
+    packages=['tablo', 'tablo.migrations', 'tablo.interfaces', 'tablo.interfaces.arcgis'],
     install_requires=['Django>=1.7.0', 'sqlparse>=0.1.18', 'pyproj', 'lxml', 'defusedxml', 'six', 'messytables'],
     url='http://github.com/consbio/tablo',
     license='BSD',
