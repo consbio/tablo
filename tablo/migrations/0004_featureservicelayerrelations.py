b6200c20327e116a7fcb8734074863e787e938cf
Daniel Harvey <daniel.harvey@consbio.org>
2016-07-26 15:15:07 -0700

    Squash tablo migrations.

--- tablo/migrations/0004_featureservicelayerrelations.py
+++ tablo/migrations/0004_featureservicelayerrelations.py
@@ -6,15 +6,19 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [('tablo', '0003_auto_20160106_1509')]
+    dependencies = [
+        ('tablo', '0003_auto_20160106_1509'),
+    ]
 
     operations = [
         migrations.CreateModel(
             name='FeatureServiceLayerRelations',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False)),
                 ('related_index', models.PositiveIntegerField(default=0)),
                 ('related_title', models.CharField(max_length=255)),
+                ('source_column', models.CharField(max_length=255)),
+                ('target_column', models.CharField(max_length=255)),
                 ('layer', models.ForeignKey(to='tablo.FeatureServiceLayer')),
             ],
         ),
