5d546bf193d30ce4192ea487f12b5b715d83c1f0
MikeTheReader <mlundin617@gmail.com>
2016-09-08 13:58:37 -0700

    Add travis and tox config

---
+++ tablo/tests/settings.py
@@ -0,0 +1,30 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    }
+}
+USE_TZ = False
+SITE_ID = 1
+SECRET_KEY = '12345'
+
+ROOT_URLCONF = 'test_app.urls'
+STATIC_URL = '/static/'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_nose',
+    'tablo',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ('--nocapture', )
