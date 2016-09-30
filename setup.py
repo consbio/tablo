2dac9ffb899eaea5f1e003b68bdc154c7a09f84c
MikeTheReader <mlundin617@gmail.com>
2016-09-30 14:03:12 -0700

    Bump version number to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tablo',
     description='A PostGIS table to feature service app for Django',
     keywords='feature service, map server, postgis, django',
-    version='1.0',
+    version='1.0.1',
     packages=['tablo', 'tablo.migrations', 'tablo.interfaces', 'tablo.interfaces.arcgis'],
     install_requires=['Django>=1.7.0', 'sqlparse>=0.1.18', 'pyproj', 'six', 'messytables', 'django-tastypie>=0.11.1', 'psycopg2'],
     url='http://github.com/consbio/tablo',
