d23ce5407e0250327977ad0bc8613321621a2fb8
MikeTheReader <mlundin617@gmail.com>
2017-02-09 13:59:37 -0800

    Fixed integer to bigint database migration to handle larger databases

--- tablo/migrations/0007_update_integer_columns_to_bigint.py
+++ tablo/migrations/0007_update_integer_columns_to_bigint.py
@@ -5,42 +5,49 @@ from django.db import migrations
 
 from tablo.models import PRIMARY_KEY_NAME
 
-sql = """
-DO
-$$
-DECLARE
-    table_name name;
-    column_name name;
-BEGIN
-FOR table_name IN
-    SELECT tablo_featureservicelayer.table FROM tablo_featureservicelayer
-LOOP
-    FOR column_name IN
-        EXECUTE format(
-            'SELECT column_name FROM information_schema.columns
-            WHERE table_name = %L AND column_name != ''{primary_key}'' AND data_type = ''{convert_from}'';',
-            table_name
+
+def convert_to_bigint(apps, schema_editor):
+    _alter_columns(apps, schema_editor)
+
+
+def convert_to_integer(apps, schema_editor):
+    _alter_columns(apps, schema_editor, convert_to='integer', convert_from='bigint')
+
+
+def _alter_columns(apps, schema_editor, convert_to='bigint', convert_from='integer'):
+
+    # For datasets that have DDL in transactions, migrations are done in a single transaction
+    # The size of the changes here will cause this to fail on larger databases (such as development and production)
+    # Break out of the atomic nature, allowing us to commit after every table
+    schema_editor.atomic.__exit__(None, None, None)
+
+    FeatureServiceLayer = apps.get_model('tablo', 'FeatureServiceLayer')
+
+    for layer in FeatureServiceLayer.objects.all():
+        table_query = (
+                'SELECT column_name FROM information_schema.columns '
+                'WHERE table_name = %s AND column_name != %s AND data_type = %s'
         )
-    LOOP
-        EXECUTE format('ALTER TABLE %I ALTER COLUMN %I TYPE {convert_to};', table_name, column_name);
-    END LOOP;
-END LOOP;
-END;
-$$
-LANGUAGE plpgsql;
-"""
+        cursor = schema_editor.connection.cursor()
+        cursor.execute(table_query, [layer.table, PRIMARY_KEY_NAME, convert_from])
+
+        for row in cursor:
+            alter_operation = 'ALTER TABLE {0} ALTER COLUMN {1} TYPE {2}'.format(layer.table, row[0], convert_to)
+            print(alter_operation)
+            schema_editor.execute(alter_operation)
+
+        schema_editor.connection.commit()
 
 
 class Migration(migrations.Migration):
+
     dependencies = [
         ('tablo', '0006_alter_geom_col_type'),
     ]
 
-    # The reverse sql is just for test in development; a reverse migration can throw an error if tables contain integer
-    # values bigger than 2147483647. So after pushing this migration to production, rolling back should be avoided.
+    # The reverse operation is just for test in development; a reverse migration can throw an error if tables contain
+    # integer values bigger than 2147483647. So after pushing this migration to production, rolling back should be
+    # avoided.
     operations = [
-        migrations.RunSQL(
-            sql=sql.format(primary_key=PRIMARY_KEY_NAME, convert_from='integer', convert_to='bigint'),
-            reverse_sql=sql.format(primary_key=PRIMARY_KEY_NAME, convert_from='bigint', convert_to='integer')
-        )
+        migrations.RunPython(convert_to_bigint, convert_to_integer)
     ]
