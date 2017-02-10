0c038b614c1ec9a68dc036e62bed0eeeceace268
MikeTheReader <mlundin617@gmail.com>
2017-02-10 15:30:17 -0800

    Added more tests for the MULTI variants of lines and polygons

--- tablo/tests/tables.sql
+++ tablo/tests/tables.sql
@@ -61,6 +61,41 @@ DO $$ BEGIN
   END LOOP;
 END $$;
 
+-- Multiline polyline table
+CREATE SEQUENCE db_multi_polyline_test_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 24
+  CACHE 1;
+
+CREATE TABLE db_multi_polyline_test
+(
+  db_id integer NOT NULL DEFAULT nextval('db_multi_polyline_test_id_seq'::regclass),
+  prop1 integer NOT NULL,
+  prop2 integer,
+  db_creator text NOT NULL,
+  db_created_date timestamp without time zone NOT NULL,
+  dbasin_geom geometry(Geometry, 3857),
+  CONSTRAINT db_multi_polyline_test_pkey PRIMARY KEY (db_id)
+);
+
+-- Inserts 10 random polylines in db_multi_polyline_test
+DO $$ BEGIN
+  FOR i IN 1..10 LOOP
+    INSERT INTO db_multi_polyline_test
+    VALUES (i, random() * 10, random() * 10, 'user1', current_timestamp,
+            ST_Transform(
+                ST_SetSrid(ST_MULTI(ST_MakeLine(
+                    ARRAY[
+                        ST_MakePoint((random() - 0.5) * 360, (random() - 0.5) * 180),
+                        ST_MakePoint((random() - 0.5) * 360, (random() - 0.5) * 180),
+                        ST_MakePoint((random() - 0.5) * 360, (random() - 0.5) * 180),
+                        ST_MakePoint((random() - 0.5) * 360, (random() - 0.5) * 180)]
+                )), 4326), 3857));
+  END LOOP;
+END $$;
+
 -- Polygon dataset
 CREATE SEQUENCE db_polygon_test_id_seq
   INCREMENT 1
@@ -83,3 +118,29 @@ CREATE TABLE db_polygon_test
 -- Inserts 10 random polygons in db_polygon_test
 INSERT INTO db_polygon_test
 SELECT db_id, random(), random(), db_creator, current_timestamp, ST_MakePolygon(ST_AddPoint(dbasin_geom, ST_StartPoint(dbasin_geom))) FROM db_polyline_test;
+
+
+-- Multi polygon table
+CREATE SEQUENCE db_multi_polygon_test_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 24
+  CACHE 1;
+
+CREATE TABLE db_multi_polygon_test
+(
+  db_id integer NOT NULL DEFAULT nextval('db_multi_polygon_test_id_seq'::regclass),
+  prop1 integer NOT NULL,
+  prop2 integer,
+  db_creator text NOT NULL,
+  db_created_date timestamp without time zone NOT NULL,
+  dbasin_geom geometry(Geometry, 3857),
+  CONSTRAINT db_multi_polygon_test_pkey PRIMARY KEY (db_id)
+);
+
+ -- Inserts 10 random multipolygons in db_polygon_test
+INSERT INTO db_multi_polygon_test
+SELECT db_id, random(), random(), db_creator, current_timestamp, ST_MULTI(ST_MakePolygon(ST_AddPoint(dbasin_geom, ST_StartPoint(dbasin_geom)))) FROM db_polyline_test;
+
+
--- tablo/tests/test_wkt.py
+++ tablo/tests/test_wkt.py
@@ -21,19 +21,31 @@ class TestWKT(TestCase):
                 self.assertEqual(wkt.from_esri_feature(wkt.to_esri_feature(p[0]), 'esriGeometryPoint'), p[1])
 
     def test_polyline(self):
+        self._test_any_polyline('db_polyline_test')
+
+    def test_multi_polyline(self):
+        self._test_any_polyline('db_multi_polyline_test')
+
+    def _test_any_polyline(self, table_name):
         with connection.cursor() as cur:
-            cur.execute('SELECT ST_AsText(dbasin_geom), ST_AsEWKT(ST_Multi(dbasin_geom)) FROM db_polyline_test')
+            cur.execute('SELECT ST_AsText(dbasin_geom), ST_AsEWKT(ST_Multi(dbasin_geom)) FROM {0}'.format(table_name))
             polylines = cur.fetchall()
             for pl in polylines:
                 self.assertEqual(wkt.from_esri_feature(wkt.to_esri_feature(pl[0]), 'esriGeometryPolyline'), pl[1])
 
     def test_polygon(self):
+        self._test_any_polygon('db_polygon_test')
+
+    def test_multi_polygon(self):
+        self._test_any_polygon('db_multi_polygon_test')
+
+    def _test_any_polygon(self, table_name):
         # ST_ForceRHR (Force Right Hand Rule) is used to make postgis result compatible with ESRI polygon orientation,
         # i.e. clockwise for exterior rings.
         # ST_Multi is used, because wkt returns multi geometries
         with connection.cursor() as cur:
             cur.execute(
-                'SELECT ST_AsText(dbasin_geom), ST_AsEWKT(ST_ForceRHR(ST_Multi(dbasin_geom))) FROM db_polygon_test'
+                'SELECT ST_AsText(dbasin_geom), ST_AsEWKT(ST_ForceRHR(ST_Multi(dbasin_geom))) FROM {0}'.format(table_name)
             )
             polygons = cur.fetchall()
             for pg in polygons:
