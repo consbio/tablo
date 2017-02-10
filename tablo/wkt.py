0e3e6cd3269f140fd3b233795e0c7dfefa9106c6
MikeTheReader <mlundin617@gmail.com>
2017-02-10 08:56:23 -0800

    Fixed esri polygons having too much nesting

--- tablo/wkt.py
+++ tablo/wkt.py
@@ -32,7 +32,7 @@ def to_esri_feature(wkt):
     elif geom_type in ['LINESTRING', 'MULTILINESTRING']:
         return {'paths': _geom_repl()}
     elif geom_type == 'POLYGON' or geom_type == 'MULTIPOLYGON':
-        return {'rings': _geom_repl()}
+        return {'rings': _geom_repl()[0]}
 
 
 def from_esri_feature(feature, geom_type):
