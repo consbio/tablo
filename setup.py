0699a8814b24bf590ede489a69e0bf42791487f7
dharvey-consbio <daniel.harvey@consbio.org>
2018-07-17 13:30:00 -0700

    Remove support for Django<1.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.1.2',
     packages=['tablo', 'tablo.migrations', 'tablo.interfaces', 'tablo.interfaces.arcgis'],
     install_requires=[
-        'Django>=1.8.0,<1.12.0', 'sqlparse>=0.1.18', 'pyproj', 'six', 'messytables',
+        'Django==1.11.*', 'sqlparse>=0.1.18', 'pyproj', 'six', 'messytables',
         'django-tastypie==0.14.*', 'psycopg2', 'Pillow>=2.9.0', 'django-storages>=1.5.2', 'boto3>=1.4.4'
     ],
     test_suite='tablo.tests.runtests.runtests',
