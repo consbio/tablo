28c0e55149efc1cdfea2b164ac9588c2017e48cc
dharvey-consbio <daniel.harvey@consbio.org>
2018-08-08 13:19:22 -0700

    Make middleware implementation Django 2.0 compatible

--- tablo/tests/settings.py
+++ tablo/tests/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = (
     'tablo',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
