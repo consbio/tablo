23a082d4035eec48f1b0aba08fcec5ea9d73b848
dharvey-consbio <daniel.harvey@consbio.org>
2020-02-12 09:05:39 -0800

    Upgrade Django for security vulnerability

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.3.0',
     packages=['tablo', 'tablo.migrations', 'tablo.interfaces', 'tablo.interfaces.arcgis'],
     install_requires=[
-        'Django>=1.11.19,<2.0', 'sqlparse>=0.1.18', 'pyproj', 'six', 'pandas==0.24.*',
+        'Django>=1.11.28,<2.0', 'sqlparse>=0.1.18', 'pyproj', 'six', 'pandas==0.24.*',
         'django-tastypie==0.14.*', 'psycopg2', 'Pillow>=2.9.0', 'django-storages>=1.5.2',
         'boto3>=1.4.4', 'sqlalchemy==1.3.*', 'geoalchemy2==0.6.*'
     ],
